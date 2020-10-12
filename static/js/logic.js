bdc343ec98cea86fbd4f0ca7d405a2faff24145a
kroddy95 <kroddy95@gmail.com>
2020-10-12 13:34:10 -0500

    Finished circles

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,5 @@
 var url ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// Grabbing our GeoJSON data..
 d3.json(url, function(data) {
     createFeatures(data.features);
 });
@@ -13,24 +12,12 @@ function createFeatures(earthquakeData) {
 
     function markerColor(depth) {
         console.log(depth)
-        if (depth > 90) {
-            return "red";
-        }
-        else if (depth > 70 && depth <= 90) {
-            return "DarkOrange";
-        }
-        else if (depth > 50 && depth <= 70) {
-            return "Orange";
-        }
-        else if (depth > 30 && depth <= 50) {
-            return "GoldenRod";
-        }
-        else if (depth > 10 && depth <= 30) {
-            return "GreenYellow";
-        }
-        else {
-            return "green";
-        }
+        if (depth > 90) {return "red"}
+        else if (depth > 70 && depth <= 90) {return "DarkOrange"}
+        else if (depth > 50 && depth <= 70) {return "Orange"}
+        else if (depth > 30 && depth <= 50) {return "GoldenRod"}
+        else if (depth > 10 && depth <= 30) {return "GreenYellow"}
+        else {return "green"}
     }
 
     function onEachFeature(feature, layer) {
@@ -63,18 +50,9 @@ function createMap(earthquakes) {
         accessToken: API_KEY
     });
 
+    var baseMaps = {"Greyscale": grayscale};
+    var overlayMaps = {"Earthquakes": earthquakes};
 
-    // Define a baseMaps object to hold our base layers
-    var baseMaps = {
-        "Greyscale": grayscale
-    };
-
-    // Create overlay object to hold our overlay layer
-    var overlayMaps = {
-        "Earthquakes": earthquakes
-    };
-
-    // Create our map, giving it the satellite and earthquakes layers to display on load
     var myMap = L.map("map", {
         center: [15.5994, -28.6731],
         zoom: 1.5,
@@ -84,12 +62,4 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
-}
-     var markers = L.circle(feature.properties.place, {
-        fillOpacity: 0.75,
-        color: "black",
-        fillColor: "purple",
-        // Setting our circle's radius equal to the output of our markerSize function
-        // This will make our marker's size proportionate to its population
-        radius: markerSize(feature.properties.mag)
-    })
+ }
